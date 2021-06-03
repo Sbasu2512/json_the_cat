4382a64c0672b1ccd2ba56892a3328a919f358b9
Sayantan Basu <sayantanbasu@live.in>
2021-06-03 18:25:51 -0400

    making more fns

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,24 @@
 const request = require('request');
 const args = process.argv.splice(2);
-let search = args[0];
+//let search = args[0];
 // console.log(search);
 // let URL = `api.thecatapi.com/v1/breeds/search?q=${search}` ;
 //const URL = `https://api.thecatapi.com/v1/breeds/search?q=${search}`;
 
- const URLmaker = (search) => {
-  return `https://api.thecatapi.com/v1/breeds/search?q=${search}`
+const search = (arr) => {
+  for(ele of arr){
+    URL(ele);
+  }
+}
+
+ const URL = (term) => {
+   const site = `https://api.thecatapi.com/v1/breeds/search?q=${term}`
+   finder(site);
  }
 
 //console.log(URL(args));
-const finder = (URL) => {
-  request(URL, (error, response, body) => {
+const finder = (url) => {
+  request(url, (error, response, body) => {
     const obj = JSON.parse(body);
     if (obj === undefined) {
       console.log("No response");
@@ -21,7 +28,9 @@ const finder = (URL) => {
   });
 };
 
-for (let ele of args) {
-  console.log(ele);
-  finder(URLmaker(ele));
-}
+search(args);
+ 
+// for (let ele of args) {
+//   console.log(ele);
+//   finder(URLmaker(ele));
+// }
