d40147b642ed768bc201f5cc7e03f50aa5dd081c
Sayantan Basu <sayantanbasu@live.in>
2021-06-03 20:52:47 -0400

    created a new file

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-const search = require('./breedFetcher');
+const {breedFetcher, search} = require('./breedFetcher');
+//const {search} = require('search');
+const args = process.argv.splice(2);
+
+
+ search(args,breedFetcher);
+
+
